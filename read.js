8d2086dd76c8cb302b1df2e017f1994b70317b3f
MuratTuz <murattuzlali@gmail.com>
2021-03-19 23:31:04 +0100

    some comments are added

--- read.js
+++ read.js
@@ -12,6 +12,8 @@ cell.printRows();
 
 http.createServer(function (req, res) {
     res.writeHead(200, { 'Content-Type': 'text/html' });
+    //if you write sheet name in the excel file to the adresse bar in the browser
+    // then it shows specified sheet in the excel file on the browser. Like localhost:8080/Sheet1
     res.write(sheet(cell.printSheet(req.url.substring(1))));
     res.end();
 }).listen(8080, 'localhost');
